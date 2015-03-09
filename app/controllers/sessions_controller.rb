f49e56d71a3698ae4318c8dc44f6ab0ae47d7d11
Steve Goldman <steve@wespeaktogether.com>
2015-03-09 15:46:10 -0400

    beginnings of log in/out

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,2 +1,24 @@
 class SessionsController < ApplicationController
+  def create
+    # find the email address
+    email_address = EmailAddress.find_by(email: params[:session][:email])
+    if email_address.nil?
+      flash.now[:danger] = 'bad email'
+      render 'new'
+    else
+      # authenticate the user
+      if email_address.user.authenticate(params[:session][:password])
+        log_in email_address.user
+        redirect_to request.referrer || root_url
+      else
+        flash.now[:danger] = 'bad password'
+        render 'new'
+      end
+    end
+  end
+
+  def destroy
+    log_out
+    redirect_to root_url
+  end
 end
--- app/helpers/sessions_helper.rb
+++ app/helpers/sessions_helper.rb
@@ -1,2 +1,23 @@
 module SessionsHelper
+
+  # logs in a user
+  def log_in(user)
+    session[:user_id] = user.id
+  end
+
+  # current logged-in user (if any)
+  def current_user
+    @current_user ||= User.find_by(id: session[:user_id])
+  end
+
+  # bool for whether a user is logged in
+  def logged_in?
+    !current_user.nil?
+  end
+
+  # logs out the current user
+  def log_out
+    session.delete(:user_id)
+    @current_user = nil
+  end
 end
