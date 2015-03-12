5e4fabdc1f35f3d06f1722271530177670147ebf
Steve Goldman <steve@wespeaktogether.com>
2015-03-12 18:10:14 -0400

    beefing up the models tests

--- test/models/email_address_test.rb
+++ test/models/email_address_test.rb
@@ -1,15 +1,35 @@
 require 'test_helper'
 
 class EmailAddressTest < ActiveSupport::TestCase
-  test "blank email" do
-    assert_not EmailAddress.new(user_id: 1, email: "   ").valid?
+
+  test "user_id and email address should be present" do
+    assert_not EmailAddress.new(            email: "hello@world.org").valid?
+    assert_not EmailAddress.new(user_id: 1).valid?
+    assert     EmailAddress.new(user_id: 1, email: "hello@world.org").valid?
   end
 
-  test "invalid email" do
-    assert_not EmailAddress.new(user_id: 1, email: "stu").valid?
+  test "email should not be blank" do
+    assert     EmailAddress.new(user_id: 1, email: "not@blank.org").valid?
+    assert_not EmailAddress.new(user_id: 1, email: "             ").valid?
   end
 
-  test "valid email" do
-    assert EmailAddress.new(user_id: 1, email: "stu@stu.com").valid?
+  test "invalid email address should not be valid" do
+    %w[@hello.com s@ s@.com.com twodots@a..com].each do |invalid_address|
+      assert_not EmailAddress.new(user_id: 1, email: invalid_address).valid?,
+                 "#{invalid_address.inspect} should not be valid"
+    end
+  end
+
+  test "valid email should be valid" do
+    %w[user@hello.com s@a.b S@com.COM 1@email.com two+s@a.c.o.m].each do |valid_address|
+      assert EmailAddress.new(user_id: 1, email: valid_address).valid?,
+             "#{valid_address.inspect} should be valid"
+    end
+  end
+
+  test "email addresses should be case-insensitively unique" do
+    email_address = EmailAddress.new(user_id: 1, email: "valid@email.org")
+    assert email_address.save
+    assert_not      EmailAddress.new(user_id: 2, email: "VaLiD@eMaIl.OrG").valid?
   end
 end
--- test/models/user_test.rb
+++ test/models/user_test.rb
@@ -4,26 +4,39 @@ class UserTest < ActiveSupport::TestCase
 
   include Constants
   
-  def setup
-    @user = User.new(name: "Example User", primary_email_address_id: nil, password: "test123", password_confirmation: "test123")
+  test "name, password, and confirmation should be present" do
+    assert_not User.new(                password: "test123", password_confirmation: "test123").valid?
+    assert_not User.new(name: "Stu G.",                      password_confirmation: "test123").valid?
+    # TODO: why does this evaluate as valid?
+    #assert_not User.new(name: "Stu G.", password: "test123").valid?
+    assert     User.new(name: "Stu G.", password: "test123", password_confirmation: "test123").valid?
   end
 
-  test "should be valid" do
-    assert @user.valid?
-  end
-
-  test "name should be present" do
-    @user.name = "     "
-    assert_not @user.valid?
+  test "name should not be blank" do
+    assert     User.new(name: "not-blank", password: "test123", password_confirmation: "test123").valid?
+    assert_not User.new(name: "         ", password: "test123", password_confirmation: "test123").valid?
   end
 
   test "name should not be too long" do
-    @user.name = "a" * (Lengths::USER_NAME_MAX + 1)
-    assert_not @user.valid?
+    not_too_long = "a" * Lengths::USER_NAME_MAX
+    assert     User.new(name: not_too_long,       password: "test123", password_confirmation: "test123").valid?
+    assert_not User.new(name: not_too_long + "a", password: "test123", password_confirmation: "test123").valid?
   end
 
-  test "password should have a minimum length" do
-    @user.password = @user.password_confirmation = "a" * (Lengths::PASSWORD_MIN - 1)
-    assert_not @user.valid?
+  test "password should not be too short" do
+    too_short = "a" * (Lengths::PASSWORD_MIN - 1)
+    assert     User.new(name: "Stu", password: too_short + "a", password_confirmation: too_short + "a").valid?
+    assert_not User.new(name: "Stu", password: too_short,       password_confirmation: too_short).valid?
+  end
+
+  test "password should match confirmation" do
+    assert     User.new(name: "discgolfstu", password: "test123", password_confirmation: "test123").valid?
+    assert_not User.new(name: "discgolfstu", password: "test123", password_confirmation: "test124").valid?
+  end
+
+  test "name should be case insensitively unique" do
+    user =     User.new(name: "discgolfstu", password: "test123", password_confirmation: "test123")
+    assert user.save
+    assert_not User.new(name: "DiScGoLfStU", password: "test123", password_confirmation: "test123").valid?
   end
 end
