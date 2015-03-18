c5a66019c0459e85330a34bbd729921507f94a74
Steve Goldman <steve@wespeaktogether.com>
2015-03-18 11:22:32 -0400

    small tests for home page

--- test/controllers/static_pages_controller_test.rb
+++ test/controllers/static_pages_controller_test.rb
@@ -1,9 +1,22 @@
 require 'test_helper'
 
 class StaticPagesControllerTest < ActionController::TestCase
-  test "should get home" do
+  def setup
+    @user = User.new(name: "Stu", password: "test123", password_confirmation: "test123")
+    @user.save!
+  end
+
+  test "home when logged out should get home" do
     get :home
     assert_response :success
+    assert_template :home
+  end
+
+  test "home when logged in should get home" do
+    log_in @user
+    get :home
+    assert_response :success
+    assert_template :home
   end
 
 end
