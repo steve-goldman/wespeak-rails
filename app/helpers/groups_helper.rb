69fe80b28f44c925a0206b744d79ddfba3539766
Steve Goldman <steve@wespeaktogether.com>
2015-03-22 21:12:43 -0400

    update flash message for previous

--- app/helpers/groups_helper.rb
+++ app/helpers/groups_helper.rb
@@ -21,7 +21,7 @@ module GroupsHelper
     NAME_NOT_PRESENT = ValidationMessage.new("Please choose a name")
     NAME_TOO_LONG    = ValidationMessage.new("Name must be less than #{Lengths::GROUP_NAME_MAX} characters")
     NAME_TAKEN       = ValidationMessage.new("That name is already in use")
-    NAME_FORMATTING  = ValidationMessage.new("Group names may only contain letters, numbers, underscores, dashes, and periods")
+    NAME_FORMATTING  = ValidationMessage.new("Group names may only contain letters, numbers, underscores, and dashes")
 
     DOMAIN_NOT_PRESENT = ValidationMessage.new("Please choose a domain")
     DOMAIN_TOO_LONG    = ValidationMessage.new("Domain must be less than #{Lengths::EMAIL_DOMAIN_MAX} characters")
