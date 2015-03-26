64c3529b1e7481394c3c109272e7fa2a527f66ec
Steve Goldman <steve@wespeaktogether.com>
2015-03-26 14:14:12 -0400

    more consistent path helper names

--- config/routes.rb
+++ config/routes.rb
@@ -3,11 +3,11 @@ Rails.application.routes.draw do
 
   # user creation
   get    'users/new',                       to: 'users#new',                        as: :new_user
-  post   'users',                           to: 'users#create',                     as: :users
+  post   'users',                           to: 'users#create',                     as: :create_user
 
   # password reset
   get    'users/password_reset',            to: 'password_resets#new',              as: :new_password_reset
-  post   'users/password_reset',            to: 'password_resets#create',           as: :password_resets
+  post   'users/password_reset',            to: 'password_resets#create',           as: :create_password_reset
   get    'users/password_reset/:id/udpate', to: 'password_resets#edit',             as: :edit_password_reset
   patch  'users/password_reset/:id',        to: 'password_resets#update',           as: :update_password_reset
 
@@ -16,9 +16,9 @@ Rails.application.routes.draw do
   patch  'users/activate_email/:id',        to: 'email_address_activations#update', as: :update_email_address_activation
 
   # log in/log out
-  get    'users/login',                     to: 'sessions#new',                     as: :login
-  post   'users/login',                     to: 'sessions#create',                  as: :logins
-  delete 'users/logout',                    to: 'sessions#destroy',                 as: :logout
+  get    'users/login',                     to: 'sessions#new',                     as: :new_session
+  post   'users/login',                     to: 'sessions#create',                  as: :create_session
+  delete 'users/logout',                    to: 'sessions#destroy',                 as: :destroy_session
 
   # user settings
   get    'users/settings',                  to: 'settings/generals#show',           as: :settings
@@ -27,9 +27,9 @@ Rails.application.routes.draw do
   patch  'users/settings/general',          to: 'settings/generals#update',         as: :update_settings_general
 
   get    'users/settings/email',                  to: 'settings/email_identities#index',   as: :settings_email_identities
-  post   'users/settings/email',                  to: 'settings/email_identities#create'
+  post   'users/settings/email',                  to: 'settings/email_identities#create',  as: :create_settings_email_identity
   get    'users/settings/email/:id/make_primary', to: 'settings/email_identities#edit',    as: :edit_settings_email_identity
-  delete 'users/settings/email/:id',              to: 'settings/email_identities#destroy', as: :settings_email_identity
+  delete 'users/settings/email/:id',              to: 'settings/email_identities#destroy', as: :destroy_settings_email_identity
 
   get    'users/settings/notifications',          to: 'settings/notifications#show',       as: :settings_notifications
   
