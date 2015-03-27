70cb612572d6b2143b2dbb19f703675df5dfa5a5
Steve Goldman <steve@wespeaktogether.com>
2015-03-26 23:16:17 -0400

    more generalizations and fixes

--- app/controllers/votes_controller.rb
+++ app/controllers/votes_controller.rb
@@ -1,6 +1,15 @@
 class VotesController < GroupPagesControllerBase
 
+  include GroupsHelper
+
   def index
+    @all_statements     = @info.group.get_all_statements(:voting, params[:page], params[:per_page] || DEFAULT_RECORDS_PER_PAGE)
+    @statement_pointers = @info.group.get_statement_pointers
+
+    respond_to do |format|
+      format.html
+      format.js
+    end
   end
 
 end
