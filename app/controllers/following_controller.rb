ddff149bab104ad189f47a0c3c03e9b1ea18481c
Steve Goldman <steve@wespeaktogether.com>
2015-03-31 21:24:11 -0400

    woops, fix

--- app/controllers/following_controller.rb
+++ app/controllers/following_controller.rb
@@ -3,7 +3,7 @@ class FollowingController < ApplicationController
   before_action :logged_in, only: [:index]
 
   def index
-    group_ids = "SELECT DISTINCT(group_id) FROM membership_histories WHERE user_id = :user_id"
+    group_ids = "SELECT group_id FROM followers WHERE user_id = :user_id"
     @following_groups = Group.paginate(page: params[:page], per_page: params[:per_page] || DEFAULT_RECORDS_PER_PAGE).where("id IN (#{group_ids})", user_id: @user.id)
     respond_to do |format|
       format.html
