0ffb7f883d852136c8ae2b211f9779dbb11fddd6
Steve Goldman <steve@wespeaktogether.com>
2015-03-31 21:08:43 -0400

    invite me

--- db/seeds.rb
+++ db/seeds.rb
@@ -29,6 +29,7 @@ group2 = user.groups_i_created.create!(name: "another_group", invitations: 5)
 group2.group_email_domains.create!(domain: "criterion.com")
 group2.group_email_domains.create!(domain: "gmail.com")
 group2.activate
+group2.send_invitation("steve.goldman@gmail.com", false)
 
 GroupUserInfo.new(group.name, nil, user).make_member_active
 
