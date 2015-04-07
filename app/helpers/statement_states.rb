01983362fec8d3551c5d9c7ac8b447f47a62711e
Steve Goldman <steve@wespeaktogether.com>
2015-04-07 15:37:09 -0500

    page descriptions

--- app/helpers/statement_states.rb
+++ app/helpers/statement_states.rb
@@ -8,6 +8,14 @@ class StatementStates
     rejected: 5,
   }
 
+  @descriptions = {
+    alive:    "These statements do not yet have enough support for a vote.  Support the statements you like!",
+    dead:     "These statements died because they did not get enough support for a vote in the alotted time.",
+    voting:   "These statements are open for voting.  Cast your votes!",
+    accepted: "These statements were voted in by the group.  They are the official group profile.",
+    rejected: "These statements were voted down by the group.",
+  }
+
   def StatementStates.[](key)
     @statement_states[key]
   end
@@ -18,6 +26,10 @@ class StatementStates
 
   def StatementStates.value?(value)
     @statement_states.value?(value)
+  end
+
+  def StatementStates.desc(key)
+    @descriptions[key]
   end
 
 end
