4361f07df278ebe02bfa6ded5e99967ed3cd3b6b
Steve Goldman <steve@wespeaktogether.com>
2015-04-13 16:19:06 -0400

    minor change to passed description

--- app/helpers/statement_states.rb
+++ app/helpers/statement_states.rb
@@ -12,7 +12,7 @@ class StatementStates
     alive:    "These statements need more support to have a vote.  Support the statements you like!",
     dead:     "These statements died because they did not get enough support in the alotted time to have a vote.",
     voting:   "These statements are open for voting.  Cast your votes!",
-    accepted: "These statements were voted in by the group.  They are the official group profile.",
+    accepted: "These statements were voted in by the group.  They make up the official group profile.",
     rejected: "These statements were voted down by the group.",
   }
 
