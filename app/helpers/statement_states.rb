d30d2800f4c5381f67f49c5f2aa0772040834580
Steve Goldman <steve@wespeaktogether.com>
2015-04-13 19:04:25 -0400

    making progress on statement metadata display

--- app/helpers/statement_states.rb
+++ app/helpers/statement_states.rb
@@ -8,12 +8,28 @@ class StatementStates
     rejected: 5,
   }
 
+  @state_syms = {
+    1 => :alive,
+    2 => :dead,
+    3 => :voting,
+    4 => :accepted,
+    5 => :rejected,
+  }
+
   @descriptions = {
     alive:    "These statements need more support to have a vote.  Support the statements you like!",
     dead:     "These statements died because they did not get enough support in the alotted time to have a vote.",
     voting:   "These statements are open for voting.  Cast your votes!",
     accepted: "These statements were voted in by the group.  They make up the official group profile.",
     rejected: "These statements were voted down by the group.",
+  }
+
+  @names = {
+    alive:    "Alive",
+    dead:     "Dead",
+    voting:   "Voting",
+    accepted: "Passed",
+    rejected: "Rejected",
   }
 
   def StatementStates.[](key)
@@ -32,4 +48,12 @@ class StatementStates
     @descriptions[key]
   end
 
+  def StatementStates.name(key)
+    @names[key]
+  end
+
+  def StatementStates.name_from_value(value)
+    @names[@state_syms[value]]
+  end
+  
 end
