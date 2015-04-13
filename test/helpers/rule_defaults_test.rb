178e63db6e52681944ab1b899d9b67f7f87a5f1f
Steve Goldman <steve@wespeaktogether.com>
2015-04-13 12:15:21 -0400

    test fix for new default

--- test/helpers/rule_defaults_test.rb
+++ test/helpers/rule_defaults_test.rb
@@ -6,7 +6,7 @@ class RuleDefaultsTest < ActiveSupport::TestCase
     assert_equal 3.days.to_i, RuleDefaults[:lifespan]
     assert_equal 5,           RuleDefaults[:support_needed]
     assert_equal 1.day.to_i,  RuleDefaults[:votespan]
-    assert_equal 40,          RuleDefaults[:votes_needed]
+    assert_equal 10,          RuleDefaults[:votes_needed]
     assert_equal 50,          RuleDefaults[:yeses_needed]
     assert_equal 5.days.to_i, RuleDefaults[:inactivity_timeout]
     assert_nil                RuleDefaults[:bogus_key]
