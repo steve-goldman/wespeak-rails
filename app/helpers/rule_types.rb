352aa082bfc465f9bbdba16240fe18d25b49505a
Steve Goldman <steve@wespeaktogether.com>
2015-04-12 23:07:23 -0400

    text to explain what the rules mean

--- app/helpers/rule_types.rb
+++ app/helpers/rule_types.rb
@@ -17,6 +17,43 @@ class RuleTypes
     inactivity_timeout: "Inactivity Timeout",
   }
 
+  @descriptions = {
+    lifespan:
+      "After this much time, a statement that does not have enough
+       support becomes a dead statement and will not have a vote.",
+
+    support_needed:
+      "A statement needs this much support to have a vote, as a
+       percentage of the active members in the group.  When
+       \"#{@display_names[:support_needed]}\" is low, the group will
+       vote on more statements.  When it is high, the group will vote
+       on fewer statements.",
+
+    votespan:
+      "This is how long group members have to cast their votes once a
+       vote begins.",
+
+    votes_needed:
+      "At least this percentage of active members must vote -- YES or
+       NO -- in order for a statement to pass.  This is one of two
+       requirements for a statement to pass.  See
+       \"#{@display_names[:yeses_needed]}\" for the other.",
+
+    yeses_needed:
+      "At least this percentage of votes cast must be YES in order for
+       a statement to pass.  This is one of two requirements for a
+       statement to pass.  See \"#{@display_names[:votes_needed]}\"
+       for the other.",
+
+    inactivity_timeout:
+
+      "An active member that does not participate in the group -- by
+       creating, supporting, or voting on a statement, or by clicking
+       \"Extend Membership\" -- becomes inactive and cannot support or
+       vote on statements until he or she becomes active again.",
+
+  }
+
   def RuleTypes.[](key)
     @rule_types[key]
   end
@@ -35,6 +72,10 @@ class RuleTypes
 
   def RuleTypes.display_name(key)
     @display_names[key]
+  end
+
+  def RuleTypes.description(key)
+    @descriptions[key]
   end
 
 end
