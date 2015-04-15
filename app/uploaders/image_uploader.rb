91468939d4f87a861683bf27f6adf100d359454f
Steve Goldman <steve@wespeaktogether.com>
2015-04-14 20:08:23 -0400

    larger thumb

--- app/uploaders/image_uploader.rb
+++ app/uploaders/image_uploader.rb
@@ -19,7 +19,7 @@ class ImageUploader < CarrierWave::Uploader::Base
 
   # Create different versions of your uploaded files:
   version :thumb do
-    process :resize_to_fit => [80, 80]
+    process :resize_to_fit => [160, 160]
   end
 
   # Add a white list of extensions which are allowed to be uploaded.
