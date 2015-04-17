448607300758473b880187aa25e327e534345d8b
Steve Goldman <steve@wespeaktogether.com>
2015-04-17 19:12:36 -0400

    reorder javascript dependencies for mysterious bootstrap issue

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -10,10 +10,10 @@
 // Read Sprockets README (https://github.com/sstephenson/sprockets#sprockets-directives) for details
 // about supported directives.
 //
+//= require bootstrap
 //= require jquery
 //= require jquery.turbolinks
 //= require jquery_ujs
-//= require bootstrap
 //= require turbolinks
 //= require tinymce-jquery
 //= require_tree .
