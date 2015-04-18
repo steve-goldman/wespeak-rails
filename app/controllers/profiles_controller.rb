a2844e74aefb33f7546f5921f62a4c389bcfb662
Steve Goldman <steve@wespeaktogether.com>
2015-04-17 22:44:10 -0400

    js error handling for votes

--- app/controllers/profiles_controller.rb
+++ app/controllers/profiles_controller.rb
@@ -5,20 +5,10 @@ class ProfilesController < GroupPagesControllerBase
   before_action :logged_in,        only: [:support, :unsupport, :vote_no, :vote_yes, :activate_member, :deactivate_member, :follow, :unfollow]
   before_action :statement_valid,  only: [:support, :unsupport, :vote_no, :vote_yes, :why_not_support_eligible, :why_not_vote_eligible]
   before_action :statement_alive,  only: [:why_not_support_eligible]
-  before_action :statement_voting, only: [:vote_no, :vote_yes,  :why_not_vote_eligibe]
-  before_action :does_not_vote_no, only: [:vote_no]
-  before_action :does_not_vote_yes,only: [:vote_yes]
+  before_action :statement_voting, only: [:why_not_vote_eligibe]
   before_action :support_eligible, only: [:support, :unsupport]
   before_action :vote_eligible,    only: [:vote_no, :vote_yes]
 
-  before_action only: [:vote_no] do
-    vote_creates_or_updates(Votes::NO)
-  end
-  
-  before_action only: [:vote_yes] do
-    vote_creates_or_updates(Votes::YES)
-  end
-  
   def show
     @support_order      = params[:support_order] ? true : false
     @all_statements     = @info.group.get_all_statements(@info.state, params[:page], params[:per_page] || DEFAULT_RECORDS_PER_PAGE, support_order: @support_order)
@@ -69,16 +59,34 @@ class ProfilesController < GroupPagesControllerBase
   end
 
   def vote_no
+    ok = @info.change_eligible? &&
+         @statement.state == StatementStates[:voting] &&
+         !@statement.user_vote(@info.user)
+
+    if ok
+      @info.make_member_active
+      vote = @statement.cast_vote(@info.user, Votes::NO)
+    end
+         
     respond_to do |format|
       format.html { redirect_to request.referer || root_url }
-      format.js
+      format.js { render 'could_not_vote.js.erb' if !ok }
     end
   end
 
   def vote_yes
+    ok = @info.change_eligible? &&
+         @statement.state == StatementStates[:voting] &&
+         !@statement.user_vote(@info.user)
+
+    if ok
+      @info.make_member_active
+      vote = @statement.cast_vote(@info.user, Votes::YES)
+    end
+         
     respond_to do |format|
       format.html { redirect_to request.referer || root_url }
-      format.js
+      format.js { render 'could_not_vote.js.erb' if !ok }
     end
   end
 
@@ -133,15 +141,6 @@ class ProfilesController < GroupPagesControllerBase
 
   def statement_voting
     redirect_to request.referer || root_url if @statement.state != StatementStates[:voting]
-    @vote = @statement.user_vote(@info.user)
-  end
-
-  def does_not_vote_no
-    redirect_to request.referer || root_url if @vote && @vote.vote == Votes::NO
-  end
-
-  def does_not_vote_yes
-    redirect_to request.referer || root_url if @vote && @vote.vote == Votes::YES
   end
 
   def support_eligible
@@ -152,12 +151,6 @@ class ProfilesController < GroupPagesControllerBase
   def vote_eligible
     redirect_with_flash(FlashMessages::NOT_VOTE_ELIGIBLE, request.referer || root_url) if
       !@info.vote_eligible?(@statement)
-  end
-
-  def vote_creates_or_updates(vote)
-    @info.make_member_active
-    vote = @statement.cast_vote(@info.user, vote)
-    redirect_with_validation_flash(vote, request.referer || root_url) if !vote.valid?
   end
 
 end
