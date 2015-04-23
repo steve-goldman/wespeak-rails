8b3f79e60234253e3e7b7f1ecd057e6fd70edcfa
Steve Goldman <steve@wespeaktogether.com>
2015-04-23 17:05:46 -0400

    new recaptcha key

--- config/initializers/recaptcha.rb
+++ config/initializers/recaptcha.rb
@@ -1,7 +1,7 @@
 if Rails.env.development?
   Recaptcha.configure do |config|
-    config.public_key  = '6LdFyQUTAAAAAG6Aw6IaVW_Fkk83vYoak3barTZ5'
-    config.private_key = '6LdFyQUTAAAAANS7OG2bfIQqzk88NJ0ol6IPaLUD'
+    config.public_key  = '6LfnxgUTAAAAAEdKXjgybC1-xhniMYTwFumVDpQp'
+    config.private_key = '6LfnxgUTAAAAAB7ueamDCZHLnyi0Lo2RnYN6fBvI'
     # Uncomment the following line if you are using a proxy server:
     # config.proxy = 'http://myproxy.com.au:8080'
     # Uncomment if you want to use the newer version of the API,
