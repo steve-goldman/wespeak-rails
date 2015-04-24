69811f084c08f7a0bbae710985893e9523b9513a
Steve Goldman <steve@wespeaktogether.com>
2015-04-23 20:04:07 -0400

    first shot at contact us page

---
+++ app/helpers/contact_us.rb
@@ -0,0 +1,27 @@
+module ContactUs
+  DATA = [
+    [ "support@wespeakapp.com",
+
+      "Send any problems or feedback you'd like us to know about here.
+      We'll do our best to get back to you quickly."
+    ],
+    
+    [ "press@wespeakapp.com",
+
+      "Send press inquiries here."
+    ],
+      
+    [ "legal@wespeakapp.com",
+
+      "Send questions for our lawyers."
+    ],
+      
+    [ "jobs@wespeakapp.com",
+
+      "Can you make a special contribution to the WeSpeak team?  We'd
+      love to meet you.  Please send some information about yourself
+      and what you want to do for WeSpeak to this address."
+    ],
+      
+  ]
+end
