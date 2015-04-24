66efb81dfe0515d5fbf56ead272c18236e18dece
Steve Goldman <steve@wespeaktogether.com>
2015-04-24 18:26:06 -0400

    mailer host root

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -64,7 +64,7 @@ Rails.application.configure do
   # Set this to true and configure the email server for immediate delivery to raise delivery errors.
   config.action_mailer.raise_delivery_errors = false
   config.action_mailer.delivery_method = :smtp
-  host = 'warm-eyrie-1227.herokuapp.com'
+  host = 'www.wespeakapp.com'
   config.action_mailer.default_url_options = { host: host }
   ActionMailer::Base.smtp_settings = {
     :address        => 'smtp.sendgrid.net',
