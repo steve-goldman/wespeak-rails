f7054ba29d20bbe48c8839c6a406bc12e1abc943
Steve Goldman <steve@wespeaktogether.com>
2015-04-27 13:50:13 -0400

    clean up contact-us language

--- app/helpers/contact_us.rb
+++ app/helpers/contact_us.rb
@@ -2,25 +2,25 @@ module ContactUs
   DATA = [
     [ "support@wespeakapp.com",
 
-      "Send any problems or feedback you'd like us to know about here.
+      "Send any problems or feedback you'd like us to know about.
       We'll do our best to get back to you quickly."
     ],
     
     [ "press@wespeakapp.com",
 
-      "Send press inquiries here."
+      "Press inquiries."
     ],
       
     [ "legal@wespeakapp.com",
 
-      "Send questions for our lawyers."
+      "Questions for our lawyers."
     ],
       
     [ "jobs@wespeakapp.com",
 
       "Can you make a special contribution to the WeSpeak team?  We'd
       love to meet you.  Please send some information about yourself
-      and what you want to do for WeSpeak to this address."
+      and what you want to do for WeSpeak."
     ],
       
   ]
